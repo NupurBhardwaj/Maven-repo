2474e17d8cb4f8abba82943cdceb35cb57c67755
Nupur Bhardwaj <nupurbhardwaj3008@gmail.com>
2020-12-21 15:16:46 +0530

    div added

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -13,11 +13,16 @@ public class Calc {
     	return num1*num2;
     }
     
+    public static int div(int num1,int num2) {
+    	return num1/num2;
+    }
+    
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       System.out.println("Sum=" +add(10,20));
       System.out.println("Difference=" +subs(60,20));
       System.out.println("Product=" +prod(30,20));
+      System.out.println("Quotient=" +div(60,20));
 	}
 
 }
