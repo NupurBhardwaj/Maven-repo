6afcba6eb0ff14abfafcbfa63612d406c406f255
Nupur Bhardwaj <nupurbhardwaj3008@gmail.com>
2020-12-21 15:10:03 +0530

    prod added

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -9,10 +9,15 @@ public class Calc {
     	return num1-num2;
     }
     
+    public static int prod(int num1,int num2) {
+    	return num1*num2;
+    }
+    
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       System.out.println("Sum=" +add(10,20));
       System.out.println("Difference=" +subs(60,20));
+      System.out.println("Product=" +prod(30,20));
 	}
 
 }
